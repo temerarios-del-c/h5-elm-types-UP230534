63c5131630a95d0e556a02f21d50a8613fa69eee
UP230534 <up230534@alumnos.upa.edu.mx>
2026-02-25 17:56:40 -0600

    Tarea 5 AINH

--- src/Helper.elm
+++ src/Helper.elm
@@ -27,3 +27,25 @@ airplaneScheduleAction status =
 airportAction : List AirplaneStatus -> List String
 airportAction estado =
     List.map airplaneScheduleAction estado
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade calificacion =
+    List.map
+        (\cal ->
+            if cal < 0 then
+                Failed
+
+            else if cal >= 7 then
+                Approved
+
+            else
+                Pending
+        )
+        calificacion
