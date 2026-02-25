796f1de284a3b67995be63e70d312d97cc7815fa
UP230534 <up230534@alumnos.upa.edu.mx>
2026-02-25 15:38:32 -0600

    Tarea 5 AINH

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,29 @@
 module Helper exposing (..)
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction estado =
+    List.map airplaneScheduleAction estado
